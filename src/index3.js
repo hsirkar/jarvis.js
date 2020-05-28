5dee3aeec1090d167fc4584bbaee3026738adaa4
Rakrish Dhakal <dhakal.rakrish@gmail.com>
2020-05-28 18:15:45 -0400

    Add some personality

--- src/index3.js
+++ src/index3.js
@@ -11,25 +11,9 @@ readline.clearScreenDown(process.stdout)
 // NLP Engine
 
 const manager = new NlpManager({ languages: ['en'] });
+const trainnlp = require('./train-nlp');
 
-// Adds the utterances and intents for the NLP
-manager.addDocument('en', 'goodbye for now', 'greetings.bye');
-manager.addDocument('en', 'bye bye take care', 'greetings.bye');
-manager.addDocument('en', 'okay see you later', 'greetings.bye');
-manager.addDocument('en', 'bye for now', 'greetings.bye');
-manager.addDocument('en', 'i must go', 'greetings.bye');
-manager.addDocument('en', 'hello', 'greetings.hello');
-manager.addDocument('en', 'hi', 'greetings.hello');
-manager.addDocument('en', 'howdy', 'greetings.hello');
-
-// Train also the NLG
-manager.addAnswer('en', 'greetings.bye', 'Till next time');
-manager.addAnswer('en', 'greetings.bye', 'see you soon!');
-manager.addAnswer('en', 'greetings.hello', 'Hey there!');
-manager.addAnswer('en', 'greetings.hello', 'Greetings!');
-
-manager.train().then(() => {
-    manager.save();
+trainnlp(manager).then(() => {
 
     // Terminal I/O
     const rl = readline.createInterface({

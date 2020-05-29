03b88efeb870818733c44264f6d40422b7d86124
Rakrish Dhakal <dhakal.rakrish@gmail.com>
2020-05-28 21:41:48 -0400

    Add Polly tts

--- src/index.js
+++ src/index.js
@@ -1,42 +1,77 @@
+const readline = require('readline');
+const { NlpManager } = require('node-nlp');
+const chalk = require('chalk');
+const ora = require('ora');
+const AWS = require('aws-sdk');
+const Speaker = require('speaker');
+const Stream = require('stream');
+
 // Clear console
 const blank = '\n'.repeat(process.stdout.rows)
-const readline = require("readline");
 console.log(blank)
 readline.cursorTo(process.stdout, 0, 0)
 readline.clearScreenDown(process.stdout)
 
-// Require chalk module for colors
-const chalk = require('chalk');
+// Load spinner
+const spinner = ora('Processing...');
+spinner.spinner = {
+    'interval': 80,
+    'frames': ['⠋','⠙','⠹','⠸','⠼','⠴','⠦','⠧','⠇','⠏']
+};
 
-// Terminal I/O
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+// TTS Engine
+AWS.config.accessKeyId = process.env.ACCESS_KEY_ID;
+AWS.config.secretAccessKey = process.env.SECRET_ACCESS_KEY;
+AWS.config.region = 'us-east-1';
+const polly = new AWS.Polly({ region: 'us-east-1' });
+
+function getSpeaker(){
+    return new Speaker({ channels: 1, bitDepth: 16, sampleRate: 16000 });
+};
+
+function speak(text){
+    const options = { OutputFormat: 'pcm', Text: text, TextType: 'text', VoiceId: 'Matthew', Engine: 'neural' }
+    polly.synthesizeSpeech(options, (err, data) => {
+        if(err)
+            console.log(err, err.stack);
+        else if (data){
+            // console.log(data.AudioStream);
+            if(data.AudioStream instanceof Buffer) {
+                let bufferStream = new Stream.PassThrough();
+                bufferStream.end(data.AudioStream);
+                bufferStream.pipe(getSpeaker());
+            }
+        } 
+    });
+}
+
+// NLP Engine
+const manager = new NlpManager({ languages: ['en'] });
+const trainnlp = require('./train-nlp');
+
+trainnlp(manager).then(() => {
+    // Terminal I/O
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    // Ask the prompt.
+    function prompt() {
+        rl.question('> ', input => {
+            spinner.start();
+            manager.process('en', input)
+                .then(res => respond(res.answer));
+        });
+    }
+
+    // Jarvis's final response
+    function respond(message) {
+        spinner.stop();
+        console.log(chalk.cyan('J: ' + message + ''));
+        speak(message);
+        prompt();
+    }
+
+    respond('Jarvis has finished booting up.');
 });
-
-// Ask the prompt.
-function prompt(){
-    rl.question("> ", input => calcIntent(input));
-}
-
-// Calculate the intent
-const intentParser = require('./intentParser');
-const container = new intentParser.IntentContainer('intentCache');
-
-container.addIntent('hello-intent', ['Hi there!', 'Hello.']);
-container.addIntent('goodbye-intent', ['See you!', 'Goodbye!']);
-container.addIntent('search-intent', ['Search for this thing on Google.']);
-
-container.train();
-
-function calcIntent(input){
-    respond(container.calcIntent(input));
-}
-
-// Jarvis's final response
-function respond(message){
-    console.log(chalk.cyan('J: ' + message + ''));
-    prompt();
-}
-
-respond("Jarvis has finished booting up.");

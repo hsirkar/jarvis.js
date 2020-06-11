cbb1f6ceaea94fa9b646271f8b638c142be29a9f
Rakrish Dhakal <dhakal.rakrish@gmail.com>
2020-06-10 22:22:37 -0400

    Add COVID skill

---
+++ skills/COVID.js
@@ -0,0 +1,80 @@
+const axios = require('axios').default;
+
+let instance;
+var countries = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anguilla',
+'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan',
+'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda',
+'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'British Virgin Islands',
+'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Cape Verde',
+'Cayman Islands', 'Chad', 'Chile', 'China', 'Colombia', 'Congo', 'Cook Islands', 'Costa Rica',
+'Cote D Ivoire', 'Croatia', 'Cruise Ship', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark',
+'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea',
+'Estonia', 'Ethiopia', 'Falkland Islands', 'Faroe Islands', 'Fiji', 'Finland', 'France',
+'French Polynesia', 'French West Indies', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar',
+'Greece', 'Greenland', 'Grenada', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea Bissau', 'Guyana',
+'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland',
+'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya',
+'Kuwait', 'Kyrgyz Republic', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein',
+'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali',
+'Malta', 'Mauritania', 'Mauritius', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat',
+'Morocco', 'Mozambique', 'Namibia', 'Nepal', 'Netherlands', 'Netherlands Antilles', 'New Caledonia',
+'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Palestine', 'Panama', 'Papua New Guinea',
+'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Reunion', 'Romania', 'Russia',
+'Rwanda', 'Saint Pierre and Miquelon', 'Samoa', 'San Marino', 'Satellite', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles',
+'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'South Africa', 'South Korea', 'Spain', 'Sri Lanka', 'St Kitts and Nevis',
+'St Lucia', 'St Vincent', 'St. Lucia', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland',
+'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Timor L\'Este','Togo','Tonga','Trinidad and Tobago','Tunisia','Turkey',
+'Turkmenistan','Turks and Caicos','Uganda','Ukraine','United Arab Emirates','United Kingdom','Uruguay','Uzbekistan',
+'Venezuela','Vietnam','Virgin Islands(US)','Yemen','Zambia','Zimbabwe'];
+
+const COVID = {
+    name: 'COVID',
+    init: (log, ask) => {
+        this.log = log;
+        this.ask = ask;
+        
+        instance = axios.create({
+            method: 'get',
+            headers: {
+                'Accept': 'application/json',
+                'Accept-Encoding': 'gzip, deflate',
+                'Accept-Language': 'en-US,en;q=0.5',
+                'Connection': 'keep-alive',
+                'Dnt': '1',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0'
+            }
+        });
+    },
+    doesHandleIntent: intentName => intentName.startsWith('covid'),
+    handleIntent: nlpRes => new Promise(resolve => {
+        const secondary = nlpRes.intent.split('.')[1];
+        switch (secondary) {
+            case 'summary':
+                instance.get('https://api.covid19api.com/summary')
+                    .then(res => {
+                        let finalItem = res.data.Global;
+                        let utterance = nlpRes.utterance.toLowerCase().replace('us', 'united states of america').replace('uk', 'united kingdom');
+
+                        for(item of res.data.Countries) {
+                            if(utterance.includes(item.Country.toLowerCase())) {
+                                finalItem = item;
+                                break;
+                            }
+                        }
+
+                        if(utterance.includes('death')) {
+                            resolve(`There are ${finalItem.TotalDeaths} total deaths, plus ${finalItem.NewDeaths} from yesterday, in ${finalItem.Country ? finalItem.Country : 'the world'}`);
+                        } else {
+                            resolve(`There are ${finalItem.TotalConfirmed} total cases, plus ${finalItem.NewConfirmed} from yesterday, in ${finalItem.Country ? finalItem.Country : 'the world'}`);
+                        }
+                    })
+                    .catch(() => resolve('Request failed'));
+                break;
+            default:
+                resolve(`I'm not sure`);
+                break;
+        }
+    })
+};
+
+module.exports = COVID;
--- skills/index.js
+++ skills/index.js
@@ -8,4 +8,5 @@ module.exports = [
     require('./Timer'),
     require('./Routines'),
     require('./Fun'),
+    require('./COVID'),
 ];

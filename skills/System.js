c13d28b5d0d58dd94d9a10574a482a3e5eb1f173
Rakrish Dhakal <dhakal.rakrish@gmail.com>
2020-06-14 13:31:04 -0400

    Finish renew

--- skills/System.js
+++ skills/System.js
@@ -5,6 +5,8 @@ const tts = require('../src/tts');
 const moment = require('moment');
 const { isYes, setEnv, list } = require('../src/util');
 const similarity = require('string-similarity');
+const fs = require('fs');
+const path = require('path');
 
 const System = {
     name: 'System',
@@ -151,20 +153,38 @@ const System = {
 
                 var matches = similarity.findBestMatch(target, products.map(p => p.toLowerCase()));
 
-                log(target);
-
                 if(matches.bestMatch.rating < 0.7) {
                     resolve(`No product named ${target} found. You can choose from ${list(products, 'and', '')}`);
                     return;
                 }
 
                 var index = matches.bestMatchIndex;
-                
                 log(`Target product: ${products[index]} (${matches.bestMatch.rating})`);
 
-                resolve('Hi');
+                this.ask(`Should I renew ${products[index]}?`, answer => {
+                    if (isYes(answer)){
+                        const variable = products[index].replace('Pro', '').trim().toUpperCase();
+                        const filePath = process.env[variable];
+                        
+                        if(filePath){
+                            const xml = fs.readFileSync(path.join(filePath, 'application.xml'));
+                            const oldTsn = require('cheerio').load(xml, { xmlMode: true })('Data[key=TrialSerialNumber]').text();
+                            const newTsn = BigInt(oldTsn) + 1n;
 
+                            log(`Changing TSN from ${oldTsn} to ${newTsn}...`);
 
+                            const newXml = xml.toString().replace(oldTsn, newTsn);
+
+                            fs.writeFileSync(path.join(filePath, 'application.xml'), newXml);
+                            resolve('Successfully completed');
+                        }else{
+                            resolve(`Path to ${products[index]} not set`);
+                        }
+                        
+                    } else {
+                        resolve('OK, cancelled');
+                    }
+                });
                 break;
             default:
                 resolve('That feature has not been implemented yet');
